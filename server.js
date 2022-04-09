c029fdf866cb9a84119adffdd48f4d416a6bdb60
jinicode <jinit2000@yahoo.com>
2022-04-10 00:20:23 +0530

    ho ja error solve yaar

--- server.js
+++ server.js
@@ -36,4 +36,3 @@ app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
