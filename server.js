25f6dd9e9f790dd3b500d4ec1f401d1961889661
jinicode <jinit2000@yahoo.com>
2022-04-09 22:26:16 +0530

    taylor sweet

--- server.js
+++ server.js
@@ -30,9 +30,9 @@ mongoose.set("useFindAndModify", false);
 
 if (1) {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("client/public"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
   });
 }
